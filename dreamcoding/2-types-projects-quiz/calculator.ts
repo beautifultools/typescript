4a024df335a90cd1759941705c3925ea54dffe38
john <lord1229@gmail.com>
2021-06-17 23:30:40 +0900

    calculator 예제 만들기

--- dreamcoding/2-types-projects-quiz/calculator.ts
+++ dreamcoding/2-types-projects-quiz/calculator.ts
@@ -1,8 +1,30 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+
+type operator = "add" | "substract" | "multiply" | "divide" | "remainder";
+const calculate = (opr: operator, num1: number, num2: number): number => {
+  const isRelatedToDivide = opr === "divide" || opr === "remainder";
+  if (isRelatedToDivide && num2 === 0) {
+    throw Error("cannot divide by 0");
+  }
+
+  switch (opr) {
+    case "add":
+      return num1 + num2;
+    case "substract":
+      return num1 - num2;
+    case "multiply":
+      return num1 * num2;
+    case "divide":
+      return num1 / num2;
+    case "remainder":
+      return num1 - num2 * Math.floor(num1 / num2);
+  }
+};
+
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
