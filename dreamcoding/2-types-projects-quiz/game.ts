2bbda9fd2432177357f47f5e994d1e0ec16bca07
john <lord1229@gmail.com>
2021-06-18 23:19:14 +0900

    typescript 연습을 위해 게임 관련 코드 작성.

--- dreamcoding/2-types-projects-quiz/game.ts
+++ dreamcoding/2-types-projects-quiz/game.ts
@@ -1,12 +1,38 @@
 /**
  * Let's make a game 🕹
  */
+type Position = {
+  x: number;
+  y: number;
+};
+
+type Command = "up" | "down" | "left" | "right";
+
+const position = {
+  x: 0,
+  y: 0,
+};
+
+function move(command: Command) {
+  if (command == "up") {
+    position.y++;
+  } else if (command == "down") {
+    position.y--;
+  } else if (command == "left") {
+    position.x--;
+  } else if (command == "right") {
+    position.x++;
+  } else {
+    throw Error("unknown command");
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
