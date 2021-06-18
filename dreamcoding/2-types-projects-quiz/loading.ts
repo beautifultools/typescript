2f025626a6c9d5ad259fe6e65ba72960c2e96292
john <lord1229@gmail.com>
2021-06-18 23:21:51 +0900

    typescript type 연습 loading 문제 해결

--- dreamcoding/2-types-projects-quiz/loading.ts
+++ dreamcoding/2-types-projects-quiz/loading.ts
@@ -3,24 +3,34 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  function printLoginState(resourceLoadState: ResourceLoadState) {
+    if (resourceLoadState.state === "loading") {
+      console.log("loading...");
+    } else if (resourceLoadState.state === "success") {
+      console.log("loaded");
+    } else {
+      console.log("no network");
+    }
+  }
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 }
